deb7744cf24fd0ab36f18643c9e57119f59d2d1c
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-05-19 13:51:50 +0200

    Update build.js

--- build.js
+++ build.js
@@ -4,17 +4,17 @@ converter = new showdown.Converter()
 
 fs.readdir(__dirname, (err, files) => {
   if (err) return console.log(err)
+  console.log("Current directory filenames:", files)
 
-  console.log("\nCurrent directory filenames:");
-  files.forEach(async file => {
+  files.filter(file => file.endsWith(".md")).forEach(file => {
     console.log(file)
     fs.readFile(file, "utf8", (err, data) => {
       if (err) return console.log("Error while reading file content: ", err)
       console.log(data)
 
-      fs.writeFile("dist/index.html", converter.makeHtml(data), (err) => {
+      fs.writeFile("dist/" + file.replace(".md", "") + ".html", converter.makeHtml(data), (err) => {
         if (err) return console.log(err)
-        console.log("File written successfully")
+        console.log("File " + file + "written successfully")
       })
     })
   })
