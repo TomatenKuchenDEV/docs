ec6fe34423736633d79232d606ed5178f444abaf
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-05-19 13:48:31 +0200

    Update build.js

--- build.js
+++ build.js
@@ -2,11 +2,20 @@ const fs = require("fs")
 const showdown = require("showdown")
 converter = new showdown.Converter()
 
-let data = converter.makeHtml("# This is a file containing a __collection__ of books.")
+fs.readdir(__dirname, (err, files) => {
+  if (err) return console.log(err)
 
-fs.writeFile("dist/index.html", data, (err) => {
-  if (err) console.log(err)
-  else {
-    console.log("File written successfully")
-  }
+  console.log("\nCurrent directory filenames:");
+  files.forEach(async file => {
+    console.log(file)
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) return console.log("Error while reading file content: ", err)
+      console.log(data)
+
+      fs.writeFile("dist/index.html", converter.makeHtml(data), (err) => {
+        if (err) return console.log(err)
+        console.log("File written successfully")
+      })
+    })
+  })
 })
