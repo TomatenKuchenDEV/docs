c8d4b76ac353d2afc1b10c896208c2fc627bee4d
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-05-20 21:08:35 +0200

    search + style

--- dist/assets/search.js
+++ dist/assets/search.js
@@ -2,7 +2,7 @@ function search(query) {
 	for (var i = 0; i < pages.length; i++) {
 		var page = pages[i]
 
-		if (page.title.toLowerCase().includes(query)) page.style.display = ""
-		else page.style.display = "none"
+		if (page.title.toLowerCase().includes(query.toLowerCase())) console.log("search: Show " + page.title)//page.style.display = ""
+		else console.log("search: Hide " + page.title)//page.style.display = "none"
 	}
 }
