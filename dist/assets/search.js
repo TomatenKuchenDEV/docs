e5201ffdf197c9fd461361d32f673f35314d8407
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-05-21 08:53:33 +0200

    style + search

--- dist/assets/search.js
+++ dist/assets/search.js
@@ -2,7 +2,18 @@ function search(query) {
 	for (var i = 0; i < pages.length; i++) {
 		var page = pages[i]
 
-		if (page.title.toLowerCase().includes(query.toLowerCase())) console.log("search: Show " + page.title)//page.style.display = ""
-		else console.log("search: Hide " + page.title)//page.style.display = "none"
+		//if (page.title.toLowerCase().includes(query.toLowerCase())) page.style.display = ""
+		//else page.style.display = "none"
 	}
+}
+
+window.onload = () => {
+	document.getElementById("search").addEventListener("focus", () => {
+		document.getElementById("dropdown").style.display = "block"
+	})
+	document.getElementById("search").addEventListener("blur", () => {
+		document.getElementById("dropdown").style.display = ""
+	})
+
+	document.getElementById("dropdown").innerHTML = pages.map(page => "<a class='nohighlight' href='" + page.url + "'><h3>" + page.title + "</h3><p>" + (page.lang == "de" ? "🇩🇪 Deutsch" : "🇺🇸 English") + "</p></a>").join("")
 }
