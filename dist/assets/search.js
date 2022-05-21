095485409af42422006381ddc868a72db92989b4
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2022-05-21 09:03:24 +0200

    fix search

--- dist/assets/search.js
+++ dist/assets/search.js
@@ -1,19 +1,22 @@
 function search(query) {
+	const elements = document.getElementById("dropdown").children
+
 	for (var i = 0; i < pages.length; i++) {
 		var page = pages[i]
+		var element = elements[i]
 
-		//if (page.title.toLowerCase().includes(query.toLowerCase())) page.style.display = ""
-		//else page.style.display = "none"
+		if (page.title.toLowerCase().includes(query.toLowerCase())) element.style.display = ""
+		else element.style.display = "none"
 	}
 }
 
 window.onload = () => {
 	document.getElementById("search").addEventListener("focus", () => {
-		document.getElementById("dropdown").style.display = "block"
-	})
-	document.getElementById("search").addEventListener("blur", () => {
 		document.getElementById("dropdown").style.display = ""
 	})
+	document.getElementById("search").addEventListener("blur", () => {
+		document.getElementById("dropdown").style.display = "none"
+	})
 
-	document.getElementById("dropdown").innerHTML = pages.map(page => "<a class='nohighlight' href='" + page.url + "'><h3>" + page.title + "</h3><p>" + (page.lang == "de" ? "🇩🇪 Deutsch" : "🇺🇸 English") + "</p></a>").join("")
+	document.getElementById("dropdown").innerHTML = pages.map(page => "<a class='nohighlight' href='" + page.url + "'>" + page.title + "</a>").join("")
 }
