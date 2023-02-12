0f09f5c7c1e398e637eba20a2a1426b2b04cd211
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2023-02-12 22:15:43 +0100

    default pages content

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,27 @@
-import styles from '../styles/Home.module.css';
+import Link from "next/link"
+import styles from "../styles/Home.module.css"
 
 export default function() {
 	return (
 		<div className={styles.container}>
+			<main>
+				<h1 className={styles.title}>
+					<a href="https://tomatenkuchen.eu">TomatenKuchen</a> Docs
+				</h1>
 
-		<main>
-			<h1 className={styles.title}>
-				Welcome to <a href="https://nextjs.org">Next.js!</a>
-			</h1>
+				<p>
+					Something is unclear? Join our <a href="https://tomatenkuchen.eu/support/">Discord Server</a>!
+				</p>
 
-			<p className={styles.description}>
-				Get started by editing <code>pages/index.js</code>
-			</p>
-
-			<div className={styles.grid}>
-				<a href="https://nextjs.org/docs" className={styles.card}>
-					<h3>Documentation &rarr;</h3>
-					<p>Find in-depth information about Next.js features and API.</p>
-				</a>
-
-				<a href="https://nextjs.org/learn" className={styles.card}>
-					<h3>Learn &rarr;</h3>
-					<p>Learn about Next.js in an interactive course with quizzes!</p>
-				</a>
-			</div>
-		</main>
-
-		<style jsx>{`
-			main {
-				padding: 5rem 0;
-				flex: 1;
-				display: flex;
-				flex-direction: column;
-				justify-content: center;
-				align-items: center;
-			}
-			code {
-				background: #fafafa;
-				border-radius: 5px;
-				padding: 0.75rem;
-				font-size: 1.1rem;
-				font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-					DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-			}
-		`}</style>
+				<div className={styles.grid}>
+					{process.env.menu.map(item => (
+						<Link href={item.link} key={item.link} className={styles.card}>
+							<h3>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h3>
+							<p>Find in-depth information about Next.js features and API.</p>
+						</Link>
+					))}
+				</div>
+			</main>
 		</div>
 	)
 }
--- pages/tutorials.js
+++ pages/tutorials.js
@@ -1,12 +1,12 @@
-import Link from "next/link";
-import styles from "../styles/Sidebar.module.css";
+import Link from "next/link"
+import styles from "../styles/Tutorials.module.css"
 
 export default function() {
 	return (
 		<div className={styles.container}>
 			{process.env.menu.map(item => (
-				<Link href={item.link}>
-					{item.name}
+				<Link href={item.link} key={item.link}>
+					{item.name.charAt(0).toUpperCase() + item.name.slice(1)}
 				</Link>
 			))}
 		</div>
